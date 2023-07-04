88b9a0961689213c0f1f767877bcc131189d198c
guipitta <137183276+guipitta@users.noreply.github.com>
2023-07-04 09:59:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,12 +24,11 @@ const displayPokemon = (pokemon) =>{
     console.log(pokemon);
     const pokemonHTMLString = pokemon.map ( pokeman => `
     <li class="card">
-        <img class="card-image" src="${pokeman.name}"/>
+        <img class="card-image" src="${pokeman.image}"/>
         <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
         <p class="card-subtitle"> Type: ${pokeman.type}</p>
     </li>
-    `)
-        .join('');
+    `).join('');
         pokedex.innerHTML = pokemonHTMLString;
 };
 fetchPokemon();
